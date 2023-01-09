28d8b3bd4a28dc55674f65d41f02eb2852523bbd
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-01-09 19:19:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,9 @@ def main():
         st.subheader("Enter input for the required info: ")
         clonesize = st.slider("Choose the clone-size: ",10,50,random.uniform(10,50),format="%d")
         bumbles = st.slider("Choose Bumbles density:  ",0,1,random.uniform(0.01,0.9),format="%f")
-        andrena = st.slider("Choose Andrena density: ",0,1,random.uniform(0.01,0.9)format="%f")
-        osmia = st.slider("Choose Osmia density:  ",0,1,random.uniform(0.01,0.9)format="%f")
-        RainingDays = st.slider("Select number of raining days: ",5,15,random.uniform(5,15)format="%d")
+        andrena = st.slider("Choose Andrena density: ",0,1,random.uniform(0.01,0.9),format="%f")
+        osmia = st.slider("Choose Osmia density:  ",0,1,random.uniform(0.01,0.9),format="%f")
+        RainingDays = st.slider("Select number of raining days: ",5,15,random.uniform(5,15),format="%d")
         Average_rainingDays = st.slider("Average number of raining days: ",0,1,random.uniform(0.1,0.9),format="%f")
 
         submit = st.form_submit_button("Predict")
