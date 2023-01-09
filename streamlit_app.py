0174b4a7b527cec0d2d1593040bb2131b847e34c
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-01-09 19:19:06 +0530

    Added range

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,12 +16,12 @@ st.markdown("<h1 style='text-align: center;'>Bluberry Pollination Simulation</h1
 def main():
     with st.form('prediction_form'):
         st.subheader("Enter input for the required info: ")
-        clonesize = st.slider("Choose the clone-size: ",10,50,format="%d")
-        bumbles = st.slider("Choose Bumbles density:  ",0,1,format="%f")
-        andrena = st.slider("Choose Andrena density: ",0,1,format="%f")
-        osmia = st.slider("Choose Osmia density:  ",0,1,format="%f")
-        RainingDays = st.slider("Select number of raining days: ",5,15,format="%d")
-        Average_rainingDays = st.slider("Average number of raining days: ",0,1,format="%f")
+        clonesize = st.slider("Choose the clone-size: ",10,50,random.uniform(10,50),format="%d")
+        bumbles = st.slider("Choose Bumbles density:  ",0,1,random.uniform(0.01,0.9),format="%f")
+        andrena = st.slider("Choose Andrena density: ",0,1,random.uniform(0.01,0.9)format="%f")
+        osmia = st.slider("Choose Osmia density:  ",0,1,random.uniform(0.01,0.9)format="%f")
+        RainingDays = st.slider("Select number of raining days: ",5,15,random.uniform(5,15)format="%d")
+        Average_rainingDays = st.slider("Average number of raining days: ",0,1,random.uniform(0.1,0.9),format="%f")
 
         submit = st.form_submit_button("Predict")
         if submit:
